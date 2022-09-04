b24155988a8a84530f9cfda1834c745bb7318aa6
sergiodsg <sergiodasilva.dev@gmail.com>
2022-09-04 19:30:14 +0000

    Updated js functions

--- src/app.js
+++ src/app.js
@@ -2,10 +2,36 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+// Generación del número aleatorio según tamaño del arreglo
+function randomNumber(size) {
+  return Math.floor(Math.random() * size);
+}
+
+// Construcción del string con la excusa
+function excuseGenerator(wordsWho, wordsAction, wordsWhat, wordsWhen) {
+  return (
+    wordsWho[randomNumber(wordsWho.length)] +
+    " " +
+    wordsAction[randomNumber(wordsAction.length)] +
+    " " +
+    wordsWhat[randomNumber(wordsWhat.length)] +
+    " " +
+    wordsWhen[randomNumber(wordsWhen.length)]
+  );
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excuse = document.getElementById("excuse");
+  excuse.innerHTML = excuseGenerator(who, action, what, when);
 };
